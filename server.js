accf1cf5b1761635528dc4302a25cdf4acfa2511
Glitch (tuong-express-lesson3) <none>
2020-04-21 09:13:20 +0000

    Lesson 3

--- server.js
+++ server.js
@@ -10,12 +10,23 @@ app.set("view engine", "pug");
 app.set("views", "./views");
 
 // https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.send("I love CodersX");
+app.get("/", (req, res) => {
+  res.send("I love CodersX");
 });
 
-app.get("/todos", (request, response) => {
-  response.render("todos");
+app.get("/todos", (req, res) => {
+  var todos = ["Đi chợ", "Nấu cơm", "Rửa chén", "Học code tại CodersX"];
+  var filtered = [...todos];
+  
+  if (req.query.q) {
+    var q = req.query.q;
+
+    filtered = todos.filter(
+      todo => todo.toLowerCase().indexOf(q.toLowerCase()) !== -1
+    );
+  }
+
+  res.render("todos", { todos: filtered });
 });
 
 // listen for requests :)
