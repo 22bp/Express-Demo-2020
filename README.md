57c5d89cdd6f00826aa86f06a683d591c850ce84
Glitch (tuong-book-management-app2) <none>
2020-04-22 05:07:51 +0000

    Book Management App - CRUD Users

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # Book Management App
 
-## Functions
-
+## CRUD Books
 - Show all books
 - Search books
 - Add book
 - Show a book
 - Update book
 - Delete book
+
+## CRUD Users
+- Show all users
+- Search users
+- Add user
+- Show user
+- Update user
+- Delete user
--- server.js
+++ server.js
@@ -1,14 +1,8 @@
 const express = require("express");
 const app = express();
-const low = require("lowdb");
-const FileSync = require("lowdb/adapters/FileSync");
 
-const adapter = new FileSync("db.json");
-const db = low(adapter);
-const shortid = require("shortid");
-
-// Set some defaults (required if your JSON file is empty)
-db.defaults({ books: [] }).write();
+const booksRoute = require("./routes/books");
+const usersRoute = require("./routes/users");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -16,81 +10,13 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "pug");
 app.set("views", "./views");
 
-var books = db.get("books").value();
-
 // Index
 app.get("/", (req, res) => {
   res.redirect("/books");
 });
 
-// Show all books
-app.get("/books", (req, res) => {
-  var filtered = [...books];
-
-  if (req.query.q) {
-    filtered = books.filter(
-      book => book.title.toLowerCase().indexOf(req.query.q.toLowerCase()) !== -1
-    );
-  }
-
-  res.render("books", { books: filtered });
-});
-
-// Show single book
-app.get("/books/:id/view", (req, res) => {
-  var book = db
-    .get("books")
-    .find({ id: req.params.id })
-    .value();
-  if (book) {
-   res.render("view-book", { book }); 
-  } else {
-    res.send('Book not found')
-  }
-});
-
-// Add book
-app.get("/books/add", (req, res) => {
-  res.render("add-book");
-});
-
-app.post("/books/add", (req, res) => {
-  if (req.body && req.body.title !== '' && req.description !== '') {
-    var newbook = req.body;
-    newbook.id = shortid.generate();
-
-    db.get("books")
-      .push(newbook)
-      .write();
-    res.redirect("/books");
-  }
-});
-
-// Edit book
-app.get("/books/:id/edit", (req, res) => {
-  var book = db.get('books').find({ id: req.params.id }).value();
-  if (book) {
-   res.render("edit-book", { book }); 
-  } else {
-    res.send('Book not found');
-  }
-});
-
-app.post('/books/:id/edit', (req, res) => {
-  db.get('books').find({ id: req.params.id }).assign(req.body).write();
-  res.redirect('/books/'+ req.params.id + '/view');
-})
-
-// Delete book
-app.get('/books/:id/delete', (req, res) => {
-  var book = db.get('books').find({ id: req.params.id }).value();
-  if (book) {
-   db.get('books').remove({ id: book.id }).write();
-    res.redirect('/books')
-  } else {
-    res.send('Book not found');
-  }
-})
+app.use("/books", booksRoute);
+app.use("/users", usersRoute);
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
